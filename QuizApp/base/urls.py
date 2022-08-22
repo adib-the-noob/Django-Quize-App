81bacf1771aa45d3d5c69d8c5d84856c462ca618
Mohammed Adib <76277079+ADIB-the-NOOB@users.noreply.github.com>
2022-08-22 19:25:19 +0600

    Boostrap Added

--- QuizApp/base/urls.py
+++ QuizApp/base/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.get_quize, name='get_quize'),
+    path('', views.home, name='home'),
+    path('quiz/', views.get_quize, name='get_quize'),
+    path('quiz/', views.quiz, name='quiz'),
 ]
--- QuizApp/base/views.py
+++ QuizApp/base/views.py
@@ -1,19 +1,37 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,redirect
 from django.http import JsonResponse
 from .models import Category, Question, Answer
 import random
 # Create your views here.
 
+def home(request):
+    context = {
+        'categories': Category.objects.all()
+    }
+
+    if request.GET.get("category"):
+        return redirect(f"/quiz/?category={request.GET.get('category')}")
+
+    return render(request, 'home.html',context)
+
+def quiz(request):
+    return render(request, 'quiz.html')
+
+
 def get_quize(request):
     try:
-        questions = list(Question.objects.all())
+        questions = (Question.objects.all())
+
+        if request.GET.get("category"):
+            questions = questions.filter(category__category_name__icontains=request.GET.get("category"))
+        questions = list(questions)
         data = []
         random.shuffle((questions))
 
-        print(questions)
+        # print(questions)
 
         for question in questions:
-            print(question.question)
+            # print(question.question)
             data.append({
                 'category': question.category.category_name,
                 'question': question.question,
@@ -27,5 +45,5 @@ def get_quize(request):
         }
         return JsonResponse(payload)
     except Exception as e:
-        print(e)
-    return HttpResponse('Something went wrong')
+        #print(e)
+        return HttpResponse('Something went wrong')
